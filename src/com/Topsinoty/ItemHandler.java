d610cb6e9aa4c131ff17e6424ff062a2d27a78ce
Promise Temitope <oluwatobilobatemi05@gmail.com>
2023-03-24 00:07:45 +0200

    Finished

--- src/com/Topsinoty/ItemHandler.java
+++ src/com/Topsinoty/ItemHandler.java
@@ -7,24 +7,35 @@ import java.util.List;
 
 public class ItemHandler {
     public List<String> itemNames = new ArrayList<>();
-    public List<Integer> numberOfItems=new ArrayList<>();
-    public List<Float> itemCosts=new ArrayList<>();
+    public List<Integer> numberOfItems = new ArrayList<>();
+    public List<Float> itemCosts = new ArrayList<>();
+    public List<Float> multipliedItemCosts = new ArrayList<>();
 
     public String showItem(String itemName, float itemCost, int itemQuantity) {
         float result = itemCost * (float) itemQuantity;
+        multipliedItemCosts(result);
         return "You have bought [" + itemName.toUpperCase() + " x" + itemQuantity + "] (" + NumberFormat.getCurrencyInstance().format(result) + ")";
     }
 
-    public List<String> itemName(String newItem) {
+    public String getTotalCosts(List<Float> totalCosts) {
+        float total = 0;
+        for (Float d : totalCosts)
+            total += d;
+        return NumberFormat.getCurrencyInstance().format(total);
+    }
+
+    public void itemName(String newItem) {
         itemNames.add(newItem);
-        return itemNames;
     }
-    public List<Integer> itemQuantity(int quantity) {
-        numberOfItems.add(quantity);
-        return numberOfItems;
-    }
-    public List<Float> itemCost(float itemCost){
+
+    public void itemCost(float itemCost) {
         itemCosts.add(itemCost);
-        return itemCosts;
+    }
+
+    public void itemQuantity(int quantity) {
+        numberOfItems.add(quantity);
+    }
+    public void multipliedItemCosts(float multiplied){
+        multipliedItemCosts.add(multiplied);
     }
 }
--- src/com/Topsinoty/Main.java
+++ src/com/Topsinoty/Main.java
@@ -2,23 +2,61 @@ package com.topsinoty;
 
 import java.util.Scanner;
 
+
 public class Main {
     public Scanner scanner = new Scanner(System.in);
     ItemHandler items = new ItemHandler();
 
+
     public static void main(String[] args) {
         Main main = new Main();
-        main.getItem();
+        boolean hasMoreItems = false;
+        do {
+            main.getItem();
+
+            System.out.print("Do you want to keep browsing? (y/n): ");
+            String yesOrNo = main.scanner.next().toLowerCase();
+
+            if (yesOrNo.equals("yes") || yesOrNo.equals("y")) {
+                hasMoreItems = true;
+            } else if (yesOrNo.equals("no") || yesOrNo.equals("n")) {
+                //showItems
+
+                main.getValueFromArray();
+//                System.out.println(main.items.itemNames);
+//                System.out.println(main.items.itemCosts);
+//                System.out.println(main.items.numberOfItems);
+                break;
+            }
+        } while (hasMoreItems);
     }
 
     public void getItem() {
+        //Post values into respective arrays
+
         System.out.print("Bought item: ");
         String itemName = scanner.next().toLowerCase();
+        items.itemName(itemName);
+
         System.out.print("How many: ");
         int itemQuantity = scanner.nextInt();
+        items.itemQuantity(itemQuantity);
+
         System.out.print("How much: £");
         float itemCost = scanner.nextFloat();
+        items.itemCost(itemCost);
 
-        System.out.println(items.showItem(itemName, itemCost, itemQuantity));
+//            System.out.println(items.itemNames);
+//            System.out.println(items.itemCosts);
+//            System.out.println(items.numberOfItems);
+
+    }
+
+    public void getValueFromArray() {
+        for (int i = 0; i < items.itemNames.toArray().length; i++) {
+            String result = items.showItem(items.itemNames.get(i), items.itemCosts.get(i), items.numberOfItems.get(i));
+            System.out.println("You have bought " + result);
+        }
+        System.out.print("Total costs: "+items.getTotalCosts(items.multipliedItemCosts));
     }
 }
